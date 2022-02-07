4f749ad9c710e5708a21b9c7f25ce4a53616b1ee
Alison Melendez <alisonmelendez132@gmail.com>
2022-02-06 21:51:32 -0500

    commit tree 58df6d23c9de529933b118ff0e538a9ddfa24aef

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+    useEffect(() => {
+      if(timeRemaining === 0){
+        setTimeRemaining(10)
+        onAnswered(false);
+        return; 
+      }
+      const timerId = setTimeout(() => {
+        console.log("running")
+        setTimeRemaining((timeRemaining) => timeRemaining - 1);
+      }, 1000);
+
+      //clearing the timer 
+      return function() {
+        clearTimeout(timerId)
+      };      
+    }, [timeRemaining,onAnswered]);
+    
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
